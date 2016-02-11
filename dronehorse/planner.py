7e9849d6f9df50859fb376df1ac6bfd52a1d9b3d
Blaine Rogers <blaine.w.rogers@gmail.com>
2016-02-11 19:06:43 +0000

    Added __init__.py and planner

---
+++ dronehorse/planner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+"""planner
+
+Creates a plan for how to move drones to maximise score.
+
+"""
+
+from .classes import State
+
+__status__ = "Development"
+__version__ = "0.0.0"
+__authors__ = ["Blaine Rogers <blaine.rogers14@imperial.ac.uk>",
+               "Elliot Steele <elliot.steele14@imperial.ac.uk>",
+               "Jonathan Sutton <jonathan.sutton14@imperial.ac.uk>"]
+
+
+
